db0d588d2d89ba95eadf8a141e97d5e4181d5990
Gustaf <wall.gustaf@gmail.com>
2016-09-14 14:21:00 +0200

    Fixed some shit in the laser code

--- AI Prototype/Assets/Scripts/PlayerController.cs
+++ AI Prototype/Assets/Scripts/PlayerController.cs
@@ -3,39 +3,42 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float m_laserTimer;
     [SerializeField] float m_laserCD;
+
 	// Use this for initialization
 	void Start ()
     {
-        m_laserCD = 0f;
+        m_laserTimer = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         //Check if left mouse button is pressed
-        if (m_laserCD <= 0f)
+        if (m_laserTimer <= 0f)
         {
             if (Input.GetMouseButton(0))
             {
-                m_laserCD = 2f;
+                m_laserTimer = m_laserCD;
                 RaycastHit m_hit;
                 Ray m_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
                 Physics.Raycast(m_ray, out m_hit);
 
                 Debug.DrawLine(Camera.main.transform.position, m_hit.point);
-                Debug.Log("Shot fired");
-                Debug.Log("Laser Cool down = " + m_laserCD);
+                Debug.Log("Shot fired at: " + m_hit.point);
             }
         }
         else
         {
-            m_laserCD -= (1 * Time.deltaTime);
+            m_laserTimer -= (1 * Time.deltaTime);
+            Debug.Log("Laser timer: " + m_laserTimer);
+
         }
 
-        if (m_laserCD <= 0)
-            m_laserCD = 0f;
+        if (m_laserTimer <= 0)
+            m_laserTimer = 0f;
 
 	}
 }
